d5c10f4386993334a36b56ff879ecc531bb2daaa
bvpav <78971592+bvpav@users.noreply.github.com>
2025-06-21 17:12:13 +0300

    feat: Complete 02 Rays: Task 1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chaos Ray Tracing Course 2025
+
+This repository contains my solutions to the assignments of the [Chaos Ray Tracing Course 2025](https://github.com/VladislavVulchevChaos/ChaosRayTracingCourse2025).
+
+| Task            | Source Code                                                                                               | Result                                                                                   |
+| :-------------- | :-------------------------------------------------------------------------------------------------------- | ---------------------------------------------------------------------------------------- |
+| 02 Rays: Task 1 | [main.cpp](https://github.com/bvpav/chaos-ray-tracing-course-2025/blob/02-01-rectangle-grid/src/main.cpp) | [![02 Rays: Task 1](results/02-01-rectangle-grid.ppm)](results/02-01-rectangle-grid.ppm) |
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <algorithm>
 
-static constexpr int IMAGE_WIDTH = 1920;
-static constexpr int IMAGE_HEIGHT = 1080;
+static constexpr int IMAGE_WIDTH = 800;
+static constexpr int IMAGE_HEIGHT = 600;
 
 static constexpr int MAX_COLOR_COMPONENT = 0xFF;
+
+static constexpr int RECTANGLE_COUNT = 4;
+static constexpr int NOISE_MIN = -MAX_COLOR_COMPONENT;
+static constexpr int NOISE_MAX = MAX_COLOR_COMPONENT / 3;
 
 int main(int argc, char *argv[]) {
     auto output_path = argc > 1 ? argv[1] : "output.ppm";
@@ -19,15 +24,27 @@ int main(int argc, char *argv[]) {
                 << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << '\n'
                 << MAX_COLOR_COMPONENT << '\n';
 
-    for (int row_idx = 0; row_idx < IMAGE_HEIGHT; ++row_idx) {
-        for (int col_idx = 0; col_idx < IMAGE_WIDTH; ++col_idx) {
-            auto r = double(col_idx) / (IMAGE_WIDTH - 1);
-            auto g = double(row_idx) / (IMAGE_HEIGHT - 1);
-            auto b = 0.25;
+    for (int screen_y = 0; screen_y < IMAGE_HEIGHT; ++screen_y) {
+        for (int screen_x = 0; screen_x < IMAGE_WIDTH; ++screen_x) {
+            int rect_h_index = screen_y / (IMAGE_HEIGHT / RECTANGLE_COUNT);
+            int rect_w_index = screen_x / (IMAGE_WIDTH / RECTANGLE_COUNT);
+            int rect_index = rect_h_index * RECTANGLE_COUNT + rect_w_index;
 
-            output_file << static_cast<int>(r * MAX_COLOR_COMPONENT) << ' '
-                        << static_cast<int>(g * MAX_COLOR_COMPONENT) << ' '
-                        << static_cast<int>(b * MAX_COLOR_COMPONENT) << ' ';
+            // Resolve an adjacently unique color for each rectangle
+            int color_index = rect_index % 6 + 1;
+            int r = (color_index & 1) ? MAX_COLOR_COMPONENT : 0;
+            int g = (color_index & 2) ? MAX_COLOR_COMPONENT : 0;
+            int b = (color_index & 4) ? MAX_COLOR_COMPONENT : 0;
+
+            int noise_r = rand() % (NOISE_MAX - NOISE_MIN + 1) + NOISE_MIN;
+            int noise_g = rand() % (NOISE_MAX - NOISE_MIN + 1) + NOISE_MIN;
+            int noise_b = rand() % (NOISE_MAX - NOISE_MIN + 1) + NOISE_MIN;
+
+            r = std::clamp(r + noise_r, 0, MAX_COLOR_COMPONENT);
+            g = std::clamp(g + noise_g, 0, MAX_COLOR_COMPONENT);
+            b = std::clamp(b + noise_b, 0, MAX_COLOR_COMPONENT);
+
+            output_file << r << ' ' << g << ' ' << b << '\n';
         }
         output_file << '\n';
     }
