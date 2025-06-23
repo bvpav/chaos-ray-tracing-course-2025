5c9522b3d8b2c187d7716e2ca5cb86f8a2607a89
bvpav <78971592+bvpav@users.noreply.github.com>
2025-06-24 01:10:16 +0300

    feat: Complete 04 Triangle 01: Task 2

--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,30 @@
 #include <cstdint>
 #include <algorithm>
 
-#include "crt_ray.h"
-
-static constexpr int RESOLUTION_X = 1920;
-static constexpr int RESOLUTION_Y = 1080;
-
-static constexpr int MAX_COLOR_COMPONENT = 0xFF;
+#include "crt_triangle.h"
 
 int main(int argc, char *argv[]) {
-    auto output_path = argc > 1 ? argv[1] : "output.ppm";
-    std::ofstream output_file(output_path, std::ios::out | std::ios::binary);
-    if (!output_file.is_open()) {
-        std::cerr << "Error: Could not open file: " << output_path << '\n';
-        return 1;
+    {
+        crt::Vector a = { 3.50f, 0.0f, 0.0f };
+        crt::Vector b = { 1.75f, 3.5f, 0.0f };
+        crt::Vector cross = a.cross(b);
+        std::cout << "AxB = (" << cross.x << ", " << cross.y << ", " << cross.z << ")\n";
     }
-
-    output_file << "P3\n"
-                << RESOLUTION_X << ' ' << RESOLUTION_Y << '\n'
-                << MAX_COLOR_COMPONENT << '\n';
-
-    for (int raster_y = 0; raster_y < RESOLUTION_Y; ++raster_y) {
-        for (int raster_x = 0; raster_x < RESOLUTION_X; ++raster_x) {
-            crt::Ray ray;
-
-            // Pixel center in raster space
-            ray.direction = { raster_x + 0.5f, raster_y + 0.5f, 0.0f };
-
-            // Raster space to NDC space
-            ray.direction.x /= RESOLUTION_X;
-            ray.direction.y /= RESOLUTION_Y;
-
-            // NDC space to screen space
-            ray.direction.x = (2.0f * ray.direction.x) - 1.0f;
-            ray.direction.y = 1.0f - (2.0f * ray.direction.y);
-
-            // Consider aspect ratio
-            ray.direction.x *= float(RESOLUTION_X) / RESOLUTION_Y;
-
-            ray.direction.z = -1.0f;
-            ray.direction = ray.direction.normalized();
-
-            ray.origin = { 0.0f, 0.0f, 0.0f };
-
-            crt::Vector color = (ray.direction * 0.5f + 0.5f) * MAX_COLOR_COMPONENT;
-            output_file << static_cast<int>(color.x) << ' '
-                        << static_cast<int>(color.y) << ' '
-                        << static_cast<int>(color.z) << ' ';
-        }
-        output_file << '\n';
+    {
+        crt::Vector a = { 3.0f, -3.0f, 1.0f };
+        crt::Vector b = { 4.0f,  9.0f, 3.0f };
+        crt::Vector cross = a.cross(b);
+        std::cout << "AxB = (" << cross.x << ", " << cross.y << ", " << cross.z << ")\n";
     }
-
-    output_file.close();
+    {
+        crt::Vector a = { 3.0f, -3.0f, 1.0f };
+        crt::Vector b = { 4.0f,  9.0f, 3.0f };
+        std::cout << "Area = " << a.cross(b).length() << '\n';
+    }
+    {
+        crt::Vector a = {   3.0f, -3.0f,  1.0f };
+        crt::Vector b = { -12.0f, 12.0f, -4.0f };
+        std::cout << "Area = " << a.cross(b).length() << '\n';
+    }
     return 0;
 }
