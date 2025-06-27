742f5760ad8b9f84e9a5b32f0097685f83ed84ed
bvpav <78971592+bvpav@users.noreply.github.com>
2025-06-28 01:27:34 +0300

    added triangle normal caching

--- src/crt_triangle.h
+++ src/crt_triangle.h
@@ -7,20 +7,29 @@ namespace crt {
 /**
  * @brief Triangle is defined by three vertices in counter-clockwise order.
  */
-struct Triangle {
-    Vector vertices[3];
+class Triangle {
+public:
+    Triangle(const Vector& v0, const Vector& v1, const Vector& v2)
+        : m_vertices{ v0, v1, v2 }
+    {
+        Vector edge0 = m_vertices[1] - m_vertices[0];
+        Vector edge1 = m_vertices[2] - m_vertices[0];
+        m_normal = edge0.cross(edge1).normalized();
+    }
 
     Vector normal() const {
-        Vector edge1 = vertices[1] - vertices[0];
-        Vector edge2 = vertices[2] - vertices[0];
-        return edge1.cross(edge2).normalized();
+        return m_normal;
     }
 
     float area() const {
-        Vector edge1 = vertices[1] - vertices[0];
-        Vector edge2 = vertices[2] - vertices[0];
-        return edge1.cross(edge2).length() * 0.5;
+        Vector edge0 = m_vertices[1] - m_vertices[0];
+        Vector edge1 = m_vertices[2] - m_vertices[0];
+        return edge0.cross(edge1).length() * 0.5;
     }
+
+private:
+    Vector m_vertices[3];
+    Vector m_normal;
 };
 
 }
